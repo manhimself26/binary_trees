a0d2fbd0d5adcdd7c94fcca8d896af834cab5fc2
manhimself26 <rewardmuchech@gmail.com>
2023-08-01 15:10:30 +0000

    binary tress

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -13,7 +13,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
  * @node: A pointer to the node to check.
  *
  * Return: If the node is a leaf, 1, otherwise, 0.
- */
+ 
 unsigned char is_leaf(const binary_tree_t *node)
 {
 	return ((node->left == NULL && node->right == NULL) ? 1 : 0);
